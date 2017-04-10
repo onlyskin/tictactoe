3a53b8f3fba9c0c897c8064ff3166550512b33c5
Sam Szreter <s.szreter@gmail.com>
2017-04-10 14:22:39 +0100

    Add integration tests for Game class
    The Game class is completely untested and needs restructuring. Added
    three integration tests to ensure that I won't change the current
    behaviour as I go about restructuring the code.

---
+++ tests/test_stdout_original_Game_class.py
@@ -0,0 +1,29 @@
+import pytest
+import sys
+
+from game import Game
+
+def test_simulated_game_1_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/simulated_stdin_1', 'r')
+	expected_stdout = open('mock/expected_stdout_1.txt', 'r').read()
+	game = Game()
+	game.start_game()
+	out, err = capsys.readouterr()
+	assert out == expected_stdout
+
+def test_simulated_game_2_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/simulated_stdin_2', 'r')
+	expected_stdout = open('mock/expected_stdout_2.txt', 'r').read()
+	game = Game()
+	game.start_game()
+	out, err = capsys.readouterr()
+	assert out == expected_stdout
+
+def test_simulated_game_3_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/simulated_stdin_3', 'r')
+	expected_stdout = open('mock/expected_stdout_3.txt', 'r').read()
+	game = Game()
+	game.start_game()
+	out, err = capsys.readouterr()
+	assert out == expected_stdout
+
