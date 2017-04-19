626f582dddec0440c09ae2b3cade6672402aeaac
Sam Szreter <s.szreter@gmail.com>
2017-04-19 14:42:21 +0100

    Change Game class self.board to use real Board object
    Changed the Game class so it uses a real Board object for it's board
    property instead of the previous behaviour where a flat array was used
    and then a Board object was instantiated every time it was needed.

--- game.py
+++ game.py
@@ -4,37 +4,31 @@ from get_best_move import get_best_move
 
 class Game:
     def __init__(self):
-        self.board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+        self.board = Board(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
         self.com = "X" # the computer's marker
         self.hum = "O" # the user's marker
 
     def start_game(self):
         # start by printing the board
-        b = Board(self.board)
-        print b
+        print self.board
         print "Enter [0-8]:"
         # loop through until the game was won or tied
         while not self.game_has_winner() and not self.is_tie():
-            b = Board(self.board)
-            human_move = get_human_move(b)
-            self.board[human_move] = self.hum
+            human_move = get_human_move(self.board)
+            self.board = self.board.move(human_move, self.hum)
             if not self.game_has_winner() and not self.is_tie():
-                b = Board(self.board)
-                computer_move = get_best_move(b, self.com, self.hum)
-                self.board[computer_move] = self.com
+                computer_move = get_best_move(self.board, self.com, self.hum)
+                self.board = self.board.move(computer_move, self.com)
 
-            b = Board(self.board)
-            print b
+            print self.board
 
         print "Game over"
 
     def game_has_winner(self):
-        board = Board(self.board)
-        return board.is_winner()
+        return self.board.is_winner()
 
     def is_tie(self):
-        board = Board(self.board)
-        return board.is_full()
+        return self.board.is_full()
 
 if __name__ == '__main__':
     game = Game()
