c7f6055338bee2188db79b968c0c95edb70ae722
Sam Szreter <s.szreter@gmail.com>
2017-04-20 17:24:54 +0100

    Remove redundant 'com' and 'hum' properties

--- game.py
+++ game.py
@@ -6,8 +6,6 @@ from computerPlayer import ComputerPlayer
 class Game:
     def __init__(self, UI, player1, player2):
         self.board = Board(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
-        self.com = "X" # the computer's marker
-        self.hum = "O" # the user's marker
         self.UI = UI
         self.player1 = player1
         self.player2 = player2
