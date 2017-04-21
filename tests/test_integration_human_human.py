3841c8ab3fe51b13aaa3fe1b2c0e8ea355d3fbb5
Sam Szreter <s.szreter@gmail.com>
2017-04-21 16:18:12 +0100

    Add integration test for human human game
    Now that it's possible for a human human game to be played and the UI
    works for this, we need an integration test for this game type.

---
+++ tests/test_integration_human_human.py
@@ -0,0 +1,15 @@
+import pytest
+import sys
+
+from game import GameMaker
+from console_UI import Console_UI
+from humanPlayer import HumanPlayer
+
+def test_simulated_h_h_game_1_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/h_h_simulated_stdin_1', 'r')
+	expected_stdout = open('mock/h_h_expected_stdout_1.txt', 'r').read()
+	consoleGameMaker = GameMaker(Console_UI)
+	game = consoleGameMaker.make_game()
+	game.start_game()
+	out, err = capsys.readouterr()
+	assert out == expected_stdout
