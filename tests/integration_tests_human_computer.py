5867cbfeae319f0a228a81da79ea97db61419bd0
Sam Szreter <s.szreter@gmail.com>
2017-04-21 14:17:19 +0100

    Rename variables and filename

--- tests/integration_tests_human_computer.py
+++ tests/integration_tests_human_computer.py
@@ -6,27 +6,27 @@ from console_UI import Console_UI
 from humanPlayer import HumanPlayer
 from computerPlayer import ComputerPlayer
 
-def test_simulated_game_1_returns_expected_stdout(capsys):
-	sys.stdin = open('mock/simulated_stdin_1', 'r')
-	expected_stdout = open('mock/expected_stdout_1.txt', 'r').read()
+def test_simulated_h_c_game_1_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/h_c_simulated_stdin_1', 'r')
+	expected_stdout = open('mock/h_c_expected_stdout_1.txt', 'r').read()
 	consoleGameMaker = GameMaker(Console_UI)
 	game = consoleGameMaker.make_game()
 	game.start_game()
 	out, err = capsys.readouterr()
 	assert out == expected_stdout
 
-def test_simulated_game_2_returns_expected_stdout(capsys):
-	sys.stdin = open('mock/simulated_stdin_2', 'r')
-	expected_stdout = open('mock/expected_stdout_2.txt', 'r').read()
+def test_simulated_h_c_game_2_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/h_c_simulated_stdin_2', 'r')
+	expected_stdout = open('mock/h_c_expected_stdout_2.txt', 'r').read()
 	consoleGameMaker = GameMaker(Console_UI)
 	game = consoleGameMaker.make_game()
 	game.start_game()
 	out, err = capsys.readouterr()
 	assert out == expected_stdout
 
-def test_simulated_game_3_returns_expected_stdout(capsys):
-	sys.stdin = open('mock/simulated_stdin_3', 'r')
-	expected_stdout = open('mock/expected_stdout_3.txt', 'r').read()
+def test_simulated_h_c_game_3_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/h_c_simulated_stdin_3', 'r')
+	expected_stdout = open('mock/h_c_expected_stdout_3.txt', 'r').read()
 	consoleGameMaker = GameMaker(Console_UI)
 	game = consoleGameMaker.make_game()
 	game.start_game()
