936283cf808879c090af8d833b0733d32b6b5a84
Sam Szreter <s.szreter@gmail.com>
2017-04-21 16:22:21 +0100

    Add second human human integration test

--- tests/test_integration_human_human.py
+++ tests/test_integration_human_human.py
@@ -13,3 +13,12 @@ def test_simulated_h_h_game_1_returns_expected_stdout(capsys):
 	game.start_game()
 	out, err = capsys.readouterr()
 	assert out == expected_stdout
+
+def test_simulated_h_h_game_2_returns_expected_stdout(capsys):
+	sys.stdin = open('mock/h_h_simulated_stdin_2', 'r')
+	expected_stdout = open('mock/h_h_expected_stdout_2.txt', 'r').read()
+	consoleGameMaker = GameMaker(Console_UI)
+	game = consoleGameMaker.make_game()
+	game.start_game()
+	out, err = capsys.readouterr()
+	assert out == expected_stdout
