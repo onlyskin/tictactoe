7215997cde494c90f777d0e3d428e2d9895102b7
Sam Szreter <s.szreter@gmail.com>
2017-04-28 19:39:35 +0100

    Update minimax to take plain Board as argument
    Update minimax function so it just takes a plain Board as its argument
    instead of needing to take a Node as it's argument which required it
    to be wrapped in another function.

--- minimax.py
+++ minimax.py
@@ -1,8 +1,7 @@
 class Node(object):
-	def __init__(self, board=None, move=None, score=None):
+	def __init__(self, move=None, score=None):
 		self.move = move
 		self.score = score
-		self.board = board
 
 	def __str__(self):
 		return 'move: ' + str(self.move) + ', score: ' + str(self.score)
@@ -21,48 +20,25 @@ def get_score(board, active_player_marker):
 		else:
 			return -10
 
-def minimax(node, active_player_marker, opponent_marker, depth=0):
-	if depth <= 1:
-		print 'active: ' + active_player_marker
-		print 'opponent: ' + opponent_marker
-	board = node.board
+def minimax(board, active_player_marker, opponent_marker, depth=0):
 
 	if board.is_winner() or board.is_full():
 		score = get_score(board, active_player_marker)
-		if depth <= 1:
-			print '\t' * depth + str(node.move) + ' scored ' + str(score)
-		# print board
 		return Node(score=score)
 
 	next_nodes = []
-	if depth == 0 or depth == 1:
-		print '\t' * depth + 'CHOICES:' + ' '.join(map(lambda x: str(x), board.get_available_positions()))
 	for position in board.get_available_positions():
 		if depth % 2 == 0:
 			new_board = board.move(position, active_player_marker)
 		else:
 			new_board = board.move(position, opponent_marker)
-		next_nodes.append(Node(board=new_board, move=position))
-	for node in next_nodes:
-		if depth == 0 or depth == 1:
-			print '\t' * depth + str(node.move)
-		score = minimax(node, active_player_marker, opponent_marker, depth + 1)
-		node.score = score.score
-
-	if depth <= 1:
-		print '\t' * depth + 'final array:'
-		for node in next_nodes:
-			print '\t' * depth + node.__str__()
+		best_node = minimax(new_board, active_player_marker, opponent_marker, depth + 1)
+		node = Node(move=position, score=best_node.score)
+		next_nodes.append(node)
 
 	if depth % 2 == 0:
 		best_node = max(next_nodes, key=maxkey)
-		# print '\t' * depth + 'BEST:' + best_node.__str__()
 		return best_node
 	else:
 		best_node = min(next_nodes, key=maxkey)
-		# print '\t' * depth + 'BEST:' + best_node.__str__()
 		return best_node
-
-def minimax_on_board(board, active_player_marker, opponent_marker):
-	root = Node(board=board)
-	return minimax(root, active_player_marker, opponent_marker, depth=0)
