48869a1c4c60fb4b36bb5e8fefeea0c9dfffea81
Sam Szreter <s.szreter@gmail.com>
2017-04-28 19:16:56 +0100

    Commit get_winner board method tests

---
+++ tests/test_get_winner_board_method.py
@@ -0,0 +1,46 @@
+import pytest
+
+from board import Board
+
+def test_it_returns_value_error_when_no_winner():
+	b = Board([None, None, None, None, None, None, None, None, None])
+	with pytest.raises(ValueError):
+		b.get_winner()
+
+def test_it_returns_X_when_left_column_all_X():
+	b = Board(['X', None, None, 'X', None, None, 'X', None, None])
+	assert b.get_winner() == 'X'
+
+def test_it_returns_X_when_middle_column_all_X():
+	b = Board([None, 'X', None, None, 'X', None, None, 'X', None])
+	assert b.get_winner() == 'X'
+
+def test_it_returns_X_when_right_column_all_X():
+	b = Board([None, None, 'X', None, None, 'X', None, None, 'X'])
+	assert b.get_winner() == 'X'
+
+def test_it_returns_O_when_left_column_all_O():
+	b = Board(['O', None, None, 'O', None, None, 'O', None, None])
+	assert b.get_winner() == 'O'
+
+def test_it_returns_true_when_top_row_all_O():
+	b = Board(['O', 'O', 'O', None, None, None, None, None, None])
+	assert b.get_winner() == 'O'
+
+def test_it_returns_true_when_second_row_all_O():
+	b = Board([None, None, None, 'O', 'O', 'O', None, None, None])
+	assert b.get_winner() == 'O'
+
+def test_it_returns_true_when_bottom_row_all_O():
+	b = Board([None, None, None, None, None, None, 'O', 'O', 'O'])
+	assert b.get_winner() == 'O'
+
+def test_it_returns_true_when_diagonal_1_is_X():
+	b = Board(['X', None, None, None, 'X', None, None, None, 'X'])
+	assert b.get_winner() == 'X'
+
+def test_it_returns_true_when_diagonal_2_is_O():
+	b = Board([None, None, 'O', None, 'O', None, 'O', None, None])
+	assert b.get_winner() == 'O'
+
+
